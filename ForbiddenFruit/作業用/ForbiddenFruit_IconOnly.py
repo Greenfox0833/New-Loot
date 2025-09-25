2ad72f36974a51e783666b914b1ad6d9c290352a
Greenfox0833 <greenfox0833@gmail.com>
2025-09-25 17:52:58 +0900

    ForbiddenFruit update 2025-09-25 17:52:58

--- ForbiddenFruit/作業用/ForbiddenFruit_IconOnly.py
+++ ForbiddenFruit/作業用/ForbiddenFruit_IconOnly.py
@@ -193,8 +193,8 @@ ONLY_WORLDLIST_KEYS = None
 ICON_SIZE = 600   # ここを 150, 600 などに変えるだけでOK
 ITEM_ICON_SCALE = 0.67  # アイテムアイコンをキャンバスに対して何倍で配置するか（0.67=約2/3）
 
-DISABLE_TEXT_AND_BAR = False
-DISABLE_RARITY_ICON  = False
+DISABLE_TEXT_AND_BAR = RUN_OPTIONS["disable_text_and_bar"]
+DISABLE_RARITY_ICON  = RUN_OPTIONS["disable_rarity_icon"]
 
 # 入力（LT/LPのFModelエクスポートJSON）
 INPUT_MINLIST_JSON = r"E:/フォートナイト/Picture/Loot Pool/TEST4/New Loot/ForbiddenFruit/作業用/items_unique_min.json"
